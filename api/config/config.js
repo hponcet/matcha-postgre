494619a88b5fa18e0336b3cfe58e9894f02ed1db
Ponpon <hugues@lalilo.co>
2017-11-16 16:02:30 +0100

    [MongoDB] Add the docker container for database

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   APP_NAME: 'Matcha',
-  PORT: 8000
+  PORT: 8000,
+  DATABASE: {
+    dialect: 'mongodb',
+    host: 'localhost',
+    port: '27017',
+    username: '9B{ps&?69P~jfAR9',
+    password: '9|~}y9ky45*}U_uN$a$SH_SC4',
+    database: 'matcha'
+  }
 }
